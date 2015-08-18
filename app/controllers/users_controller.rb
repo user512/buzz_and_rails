7c7704c73b50b2130a750dd3fbae97cee66b5c1b
Emilio Corpus <emiliocorpus@gmail.com>
2015-08-18 12:30:26 -0700

    BCrypt now working for user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+	def create
+  		@user = User.new(user_params)
+ 	
+	  if @user.save!
+	    redirect_to @user
+	  else
+	    redirect_to root_path
+	  end
+	end
+ 
+	private
+	  def user_params
+	    params.require(:user).permit(:username, :password_digest)
+	  end
+
 end
